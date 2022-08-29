bf159ff445189d3661e0ae0ebb1daea1c5412d0f
123nishan <nishan_98@hotmail.com>
2022-08-29 23:27:05 +0100

    added csv files and py files for size prediction

---
+++ Size/male/size_constant.py
@@ -0,0 +1,46 @@
+input_size=5
+hidden_size=128
+um_epochs=5
+learning_rate=0.001
+
+#demo
+subject_number = 'Subject Number'
+age = 'Age (Years)'
+gender = 'Gender'
+height = 'Reported Height (cm)'
+weight = 'Reported Weight (kg)'
+shoe_size = 'Shoe Size NL'
+shoe_size_italy="Shoe Size IT"
+pant_size='Pants Size Waist'
+jacket_size='Jacket Size'
+jackat_size_italy='Jacket Size IT'
+#measurements
+# waist = 'Waist Circumference, Pref (mm)'
+# neck='Neck Base Circumference (mm)'
+# hip= 'Hip Circumference, Maximum (mm)'
+# chest='Chest Circumference (mm)'
+# bust='Bust/Chest Circumference Under Bust (mm)'
+# crotch_height='Crotch Height (mm)'
+
+# female_chest='Chest Girth (Chest Circumference at Scye) (mm)'
+# chest_italy='Chest Girth at Scye (Chest Circumference at Scye) (mm)'
+# upper_chest='Chest Girth (Chest Circumference at Scye) (mm)'
+# malleolus_outer='Ankle Ht Rt (Malleolus, Lateral) (mm)'
+# malleolus_inner='Malleolus Med Rt (mm)'
+# measured_weight='Weight (kg)'
+# thigh='Thigh Circumference (mm)'
+# shoulder_breadth='Shoulder Breadth (mm)'
+# waist_floor='Waist Height, Preferred (mm)'
+# arm_length='Arm Length (Shoulder to Wrist) (mm)'
+#
+# outer_inseam='Outer Inseam'
+# inner_inseam='Inner Inseam'
+# inverted_triangle='Inverted Triangle'
+# rectangle='Rectangle'
+# triangle='Triangle'
+
+# ,inverted_triangle,rectangle,triangle
+# female_measurement=[waist,chest,bust,neck,hip,crotch_height,thigh,shoulder_breadth,waist_floor,arm_length,upper_chest,malleolus_outer,malleolus_inner]
+demographic = [ age, height, weight, shoe_size]
+size=[jacket_size,pant_size]
+# measurement = [ waist,chest,neck,hip,crotch_height,thigh,shoulder_breadth,waist_floor,arm_length,upper_chest,malleolus_outer,malleolus_inner]
---
+++ Size/male/size_data.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from torch.utils.data import DataLoader
+from sklearn import preprocessing
+from pickle import dump
+
+from Size.male import size_constant, size_dataloader
+
+
+def handle_split_data(country):
+
+    X_train=pd.read_csv("./" + country +"/X_train.csv", skipinitialspace=True, usecols=size_constant.demographic)
+    y_train=pd.read_csv("./" + country +"/y_train.csv", skipinitialspace=True, usecols=size_constant.size)
+
+    X_val=pd.read_csv("./" + country +"/X_val.csv", skipinitialspace=True, usecols=size_constant.demographic)
+    y_val=pd.read_csv("./" + country +"/y_val.csv", skipinitialspace=True, usecols=size_constant.size)
+
+    X_test=pd.read_csv("./" + country +"/X_test.csv", skipinitialspace=True, usecols=size_constant.demographic)
+    y_test=pd.read_csv("./" + country +"/y_test.csv", skipinitialspace=True, usecols=size_constant.size)
+
+    scaler=preprocessing.StandardScaler()
+    output_scaler=preprocessing.StandardScaler()
+
+    scaler.fit(X_train.values)
+    output_scaler.fit(y_train.values)
+
+    X_train_scaled=scaler.transform(X_train.values)
+    y_train_scaled=output_scaler.transform(y_train.values)
+
+    X_val_scaled=scaler.transform(X_val.values)
+    y_val_scaled=output_scaler.transform(y_val.values)
+
+    X_test_scaled=scaler.transform(X_test.values)
+    # y_test_scaled=output_scaler.transform(y_test.values)
+
+    X_train_scaled_data = pd.DataFrame(X_train_scaled, columns=X_train.columns)
+    X_val_scaled_data = pd.DataFrame(X_val_scaled, columns=X_train.columns)
+    X_test_scaled_data = pd.DataFrame(X_test_scaled, columns=X_train.columns)
+
+    Train_data = pd.concat([X_train_scaled_data, pd.DataFrame(y_train_scaled, columns=y_train.columns)], axis=1)
+    Valid_data = pd.concat([X_val_scaled_data, pd.DataFrame(y_val_scaled, columns=y_train.columns)], axis=1)
+    Test_data= pd.concat([X_test_scaled_data, pd.DataFrame(y_test.values, columns=y_train.columns)], axis=1)
+
+    train_data = size_dataloader.CustomDataset(Train_data)
+    val_data = size_dataloader.CustomDataset(Valid_data)
+    test_data = size_dataloader.CustomDataset(Test_data)
+
+    train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=False)
+    val_loader = DataLoader(dataset=val_data, batch_size=32, shuffle=False)
+    test_loader = DataLoader(dataset=test_data, batch_size=32, shuffle=False)
+
+    dump(scaler, open('../size_scaler_dutch.pkl', 'wb'))
+    dump(output_scaler, open('../size_output_scaler_dutch.pkl', 'wb'))
+
+    return train_loader, val_loader, test_loader, size_constant.demographic, size_constant.size, scaler, output_scaler
+
